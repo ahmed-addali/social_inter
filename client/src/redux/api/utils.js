6f06fb39de491d910a517ab7a92a62510b5d65a2
ahmed-addali <aaddali.ahmed@gmail.com>
2025-04-18 16:48:03 +0100

    Fix CORS issues with preflight requests

---
+++ client/src/redux/api/utils.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+// Ensure there's no trailing slash in the base URL
+const BASE_URL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL.replace(/\/$/, '') : 'https://social-inter-ugp4.vercel.app';
+const ADMIN_URL = `${BASE_URL}/admin`;
+
+const authInterceptor = (req) => {
+  const accessToken = JSON.parse(localStorage.getItem("profile"))?.accessToken;
+  if (accessToken) {
+    req.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return req;
+};
+
+const adminAuthInterceptor = (req) => {
+  const accessToken = JSON.parse(localStorage.getItem("admin"))?.accessToken;
+  if (accessToken) {
+    req.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return req;
+};
+
+export const API = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const ADMIN_API = axios.create({
+  baseURL: ADMIN_URL,
+});
+
+export const COMMUNITY_API = axios.create({
+  baseURL: BASE_URL,
+});
+
+API.interceptors.request.use(authInterceptor);
+ADMIN_API.interceptors.request.use(adminAuthInterceptor);
+COMMUNITY_API.interceptors.request.use((req) => {
+  req.headers["Content-Type"] = "application/json";
+  return authInterceptor(req);
+});
+
+export const handleApiError = async (error) => {
+  try {
+    const errorMessage =
+      error.response?.data?.message || "An unexpected error occurred.";
+    const data = null;
+    return { error: errorMessage, data };
+  } catch (err) {
+    throw new Error("An unexpected error occurred.");
+  }
+};
